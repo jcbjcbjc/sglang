731b8022da331efce7fb5a80ab3856c4dc11d362
jcbjcbjc <jiachaobo458@gmail.com>
2025-02-15 10:36:28 -0600

    init commit

---
+++ lora.py
@@ -0,0 +1,31 @@
+# launch server
+# python -m sglang.launch_server --model mistralai/Mistral-7B-Instruct-v0.3 --lora-paths /home/ying/test_lora lora1=/home/ying/test_lora_1 lora2=/home/ying/test_lora_2 --disable-radix --disable-cuda-graph --max-loras-per-batch 4
+
+# send requests
+# lora_path[i] specifies the LoRA used for text[i], so make sure they have the same length
+# use None to specify base-only prompt, e.x. "lora_path": [None, "/home/ying/test_lora"]
+import json
+
+import requests
+
+url = "http://141.142.254.26:30000"
+json_data = {
+    "text": [
+        "prompt 1",
+        "prompt 2",
+        "prompt 3",
+        "prompt 4",
+    ],
+    "sampling_params": {"max_new_tokens": 32},
+    "lora_path": [
+        "lora1",
+        "lora2",
+        "lora1",
+        "lora2",
+    ],
+}
+response = requests.post(
+    url + "/generate",
+    json=json_data,
+)
+print(json.dumps(response.json()))
